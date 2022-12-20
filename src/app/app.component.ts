6c3361027037c110412d42ad494ba8fc65410f11
ivanroscuenca <ivanros@protonmail.com>
2022-12-20 19:18:53 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,19 @@ export class AppComponent {
   resultado:number=0;
 
   sumar():void{
-    this.resultado=this.numero1+this.numero2;
+    this.resultado=Number(this.numero1)+Number(this.numero2);
+  }
+
+  restar():void{
+    this.resultado=Number(this.numero1)-Number(this.numero2);
+  }
+
+  multiplicar():void{
+    this.resultado=Number(this.numero1)*Number(this.numero2);
+  }
+
+  dividir():void{
+    this.resultado=Number(this.numero1)/Number(this.numero2);
   }
 
 
